de9990e37fb37139ab6b90b854e6a970e101126f
Fredrik <fredrik.wallstrom.1992@gmail.com>
2017-01-29 19:15:10 +0100

    The HUD is now updated with the right information. The health bar and also the score is placed on the right place in the screen. The HUD class is also commented.

--- core/src/com/mygdx/game/scenes/Hud.java
+++ core/src/com/mygdx/game/scenes/Hud.java
@@ -1,10 +1,13 @@
 package com.mygdx.game.scenes;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Disposable;
@@ -13,38 +16,29 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MageManBros;
 
 /**
- * Created by fredr on 2016-11-10.
+ * This class will represent the game information.
+ * Game information means the status of the player, health bar and also the score the player has.
  */
-
 public class Hud implements Disposable {
     public Stage stage;
-    private Viewport viewport;
-    private Integer score;
-    private Label scoreLabel;
-    private Label levelLabel;
-    private Label worldLabel;
-    private Label marioLabel;
 
     public Hud(SpriteBatch sb){
-        score = 0;
-
-        viewport = new FitViewport(MageManBros.V_WIDTH, MageManBros.V_HEIGHT, new OrthographicCamera());
+        // Setup the HUD viewport using a new camera separate from game cam.
+        // Define stage using that viewport and games spriteBatch.
+        Viewport viewport = new FitViewport(MageManBros.V_WIDTH, MageManBros.V_HEIGHT, new OrthographicCamera());
         stage = new Stage(viewport, sb);
 
+        // Define a table used to organize hud's labels.
         Table table = new Table();
         table.top();
         table.setFillParent(true);
 
-        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        levelLabel = new Label("00000000" , new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        worldLabel = new Label("SCORE" , new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        marioLabel = new Label("LIFE" , new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        // Define the HP-bar and the score label and place them on the table.
+        Image healthBar = new Image(new Texture(Gdx.files.internal("rock_bar.png")));
+        Label score = new Label("00000000", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
-        table.add(marioLabel).padLeft(10).padTop(10);
-        table.add(worldLabel).expandX().padTop(10);
-        table.row();
-        table.add(scoreLabel).padLeft(10);
-        table.add(levelLabel).expandX();
+        table.add(healthBar).padLeft(5).size(100, 20).padTop(10);
+        table.add(score).expandX();
 
         stage.addActor(table);
     }
